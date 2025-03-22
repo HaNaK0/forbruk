77ce1a2747173699d02ce049713d2aba1c122676
HaNaK0 <hampus@hampushuledal.com>
2025-03-22 12:17:24 +0100

    version 0.1.0
    first version, includes the abbiliy to set the current boat and add
    items to that boat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,117 @@
+use std::{
+    fs::{self, File, OpenOptions},
+    io::{BufReader, Write},
+    path::{Path, PathBuf},
+};
+
+use anyhow::{Context, anyhow};
+use chrono::Local;
+use clap::{Args, Parser, Subcommand, ValueEnum};
+use ron::ser::PrettyConfig;
+use serde::{Deserialize, Serialize};
+
+/// A simple program to track how much stuff is used
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    sub_command: Commands,
+}
+
+/// Subcommands for the application
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Add an item that has been opened
+    Add {
+        item: InventoryItem,
+        #[arg(default_value_t = 1)]
+        ammount: i8,
+    },
+    /// Set the settings stored in settings.ron
+    Set(Settings),
+}
+
+/// An enum with the different values that can be added
+#[derive(Debug, ValueEnum, Clone, Copy)]
+enum InventoryItem {
+    Milk,
+    Coffe,
+    Mugs,
+    Sugar,
+    Sticks,
+}
+
+#[derive(Args, Clone, Debug, Deserialize, Serialize)]
+struct Settings {
+    /// Set the current boat
+    #[arg(short, long)]
+    boat: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    
+    let result = match &args.sub_command {
+        Commands::Add { item, ammount } => add(item, ammount),
+        Commands::Set(settings) => set(settings),
+    };
+
+    if let Err(err) = result {
+        println!("Failed to {:?}: {}", args.sub_command, err)
+    }
+}
+
+fn load_settings() -> anyhow::Result<Settings> {
+    let path = Path::new("settings.ron");
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    Ok(ron::de::from_reader(reader)?)
+}
+
+fn add(item: &InventoryItem, ammount: &i8) -> anyhow::Result<()> {
+    let settings = load_settings()?;
+    let mut path = PathBuf::new();
+    path.push("data");
+    if !path.exists() {
+        fs::create_dir_all(&path)?;
+    }
+
+    path.push(
+        settings
+            .boat
+            .ok_or(anyhow!("No boat is set!, use set -b to set a boat"))?,
+    );
+    path.set_extension("csv");
+    let mut f = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(&path)
+        .with_context(|| format!("failed to open {:?}", &path))?;
+
+    writeln!(f, "{},{:?},{}", Local::now(), item, ammount)?;
+    Ok(())
+}
+
+fn set(settings: &Settings) -> anyhow::Result<()> {
+    let path = Path::new("settings.ron");
+
+    let settings: Settings = if path.exists() {
+        let read_settings: Settings = load_settings()?;
+
+        Settings {
+            boat: if let Some(boat) = &settings.boat {
+                Some(boat.clone())
+            } else {
+                read_settings.boat
+            },
+        }
+    } else {
+        settings.clone()
+    };
+
+    let result = ron::ser::to_string_pretty(&settings, PrettyConfig::default())?;
+
+    File::create(path)?.write_all(result.as_bytes())?;
+    Ok(())
 }
